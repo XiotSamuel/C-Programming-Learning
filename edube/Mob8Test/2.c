fded0d502c65e5a2e5621f3c93ae31a085e9df0f
XiotSamuel <samuelmoktm@gmail.com>
2022-04-01 18:30:16 +0800

    update

--- edube/Mob8Test/2.c
+++ edube/Mob8Test/2.c
@@ -4,12 +4,17 @@
 #define F2(X) ((X)*(X))
 #define F3(X) ((X)*X)
 
+
 int main(void){
-    int i =1;
+    int i=1;
     int j=2;
-    int k=3;
+    int k=5;
     int s;
-    s=F1(i+j)+F2(i-j)+F3(i+k);
-    printf("%d",s);
+    //s=F1(i+j)+F2(i-j)+F3(i+k);
+    printf("%d\n",F1(j+k));
+    //printf("%d\n",F2(i-j));
+    //printf("%d\n",F3(i+k));
+
+    //printf("%d",s);
     return 0;
 }
