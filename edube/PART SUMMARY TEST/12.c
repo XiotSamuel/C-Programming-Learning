1fa987a6f4a0a7bdc92213c357c1fa3393ee9e38
Samuel <samuelmoktm@gmail.com>
2022-04-16 16:51:43 +0800

    update

--- edube/PART SUMMARY TEST/12.c
+++ edube/PART SUMMARY TEST/12.c
@@ -1,13 +1,15 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int fun()
-{
-    int a;
-    return ++a;
-}
+// int fun()
+// {
+//     int a;
+//     return ++a;
+// }
 
-int main()
-{
-    printf("%d", fun());
-    return 0;
-}
+// int main()
+// {
+//     printf("%d", fun());
+//     return 0;
+// }
+
+which of the following strings is a legal variable name ?
--- edube/PART SUMMARY TEST/9.c
+++ edube/PART SUMMARY TEST/9.c
@@ -1,15 +1,27 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(void)
-{
-    int i, j;
-    i = 1;
-    j = 1;
-    while (i > 16)
-    {
-        i += 4;
-        j++;
-    }
-    printf("%d", j);
-    return 0;
-}
+// int main(void)
+// {
+//     int i, j;
+//     i = 1;
+//     j = 1;
+//     while (i > 16)
+//     {
+//         i += 4;
+//         j++;
+//     }
+//     printf("%d", j);
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(void)
+// {
+//     int i = 1, j = 0, k;
+//     k = (i >> j) + (j >> i) + (i >> i) + (j >> j);
+//     k <<= i;
+//     printf("%d", k);
+//     return 0;
+// }
+
+which is the correct floating point number
